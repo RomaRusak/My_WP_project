6daa0be67de76dbc37b5cc6637fcd693f9143f38
Rusak Raman <romarusakr96@gmail.com>
2025-06-05 09:08:04 +0300

    implement acf field with image type

--- wp-content/plugins/myPlugin/inc/taxonomies/CustomTaxonomyOne.php
+++ wp-content/plugins/myPlugin/inc/taxonomies/CustomTaxonomyOne.php
@@ -7,16 +7,51 @@ require_once plugin_dir_path(__FILE__) . '../interfaces/TaxonomyCustFieldsEditab
 class CustomTaxonomyOne implements TaxonomyCreatableInterface, TaxonomyCustFieldsCreateableInterface, TaxonomyCustFieldsEditableInterface{
 
     private $custom_taxonomy_name = 'custom-taxonomy-1';
-    private $custom_field2_name = 'field_2';
+    private $custom_field1_name = 'taxonomy1_field_1';
+    private $custom_field2_name = 'taxonomy1_field_2';
 
     public function init() {
+
+        
         add_action('init', [$this, 'create_taxonomy']);
+
+        add_action( 'acf/init', [$this, 'add_custom_acf_fields']);
+
         add_action($this->custom_taxonomy_name  . '_add_form_fields', [$this, 'add_meta_box']);
         add_action($this->custom_taxonomy_name . "_edit_form_fields", [$this, 'edit_custom_fields']);
 
         add_action('created_' . $this->custom_taxonomy_name , [$this, 'save_meta_box'], 10, 1);
         add_action('edited_' . $this->custom_taxonomy_name , [$this, 'save_meta_box'], 10, 1);
     }
+
+    function add_custom_acf_fields() {
+        acf_add_local_field_group([
+            'key'    => 'acf_img_field',
+            'title'  => 'Custom Field for Taxonomy',
+            'fields' => [
+                    [
+                        'key' => 'taxonomy1_field_1_image',
+                        'label' => 'Image Field',
+                        'name' => $this->custom_field1_name,
+                        'type' => 'image',
+                        'return_format' => 'url',
+                        'min_width' => 0,
+                        'max_height' => 2000,
+                    ],
+            ],
+            'location' => [
+                    [
+                        [
+                            'param' => 'taxonomy',
+                            'operator' => '==',
+                            'value' => $this->custom_taxonomy_name,
+                        ],
+                    ],
+                ],
+            
+        ]);
+    }
+
 
     public function create_taxonomy() {
         $args = [
@@ -46,6 +81,12 @@ class CustomTaxonomyOne implements TaxonomyCreatableInterface, TaxonomyCustField
                 <?php if ($custom_taxonomy1_field2['value']) {echo 'checked';}?>
                 >
             </div>
+            <!-- <div class="form-field" style="display: flex; align-items: center; gap: 10px">
+                <label for="image_field">
+                    Image field
+                </label>
+                <input type="file" name="image_field" id="image_field"/>
+            </div> -->
         <?php
     }
 
